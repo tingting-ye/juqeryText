51c7d8fa57a518d5419b32b4b978aef135fa5d8a
mc <mc@mcdeMacBook-Pro.local>
2018-03-19 21:49:00 +0800

    新增

---
+++ js/window_VirtualBuilding.js
@@ -0,0 +1,425 @@
+var xnjfSource = {
+  list: [ // 单元
+    {
+      dy: 1,
+      dyjc: "一单一单元一单元元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 2,
+      dyjc: "二单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 3,
+      dyjc: "三单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 4,
+      dyjc: "四单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 5,
+      dyjc: "五单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 6,
+      dyjc: "6单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 7,
+      dyjc: "7单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 8,
+      dyjc: "8单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 9,
+      dyjc: "9单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 10,
+      dyjc: "0单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 11,
+      dyjc: "11单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+    {
+      dy: 12,
+      dyjc: "12单元",
+      floors: [ // 楼幢
+        {
+          floor: 1, // 一层
+          rooms: [
+            { fjid: 948343, roomname: "101室", location: 1 },
+            { fjid: 948344, roomname: "103室", location: 2 },
+            { fjid: 948345, roomname: "104室", location: 3 },
+          ]
+        },
+        {
+          floor: 2,// 二层
+          rooms: [
+            { fjid: 948343, roomname: "201室", location: 1 },
+            { fjid: 948344, roomname: "203室", location: 2 },
+            { fjid: 948345, roomname: "204室", location: 3 },
+          ]
+        }
+      ]
+    },
+  ]
+}
+
+var state = {
+  nowCut: 1,//当前被选中的tab值
+}
+
+$(document).ready(function () {
+  getData()
+})
+
+function getData() {
+  getDataSource()
+}
+
+// 获取虚拟建房所有数据
+function getDataSource() {
+  tabTitle()
+  tabContent()
+  buttonEvent()
+}
+
+// tab标题切换
+function tabTitle() {
+  var data = xnjfSource.list
+  $(".unit_cut_title ul").empty()
+  var html = ""
+  data.map(function (item, i) {
+    if (i < 10) {
+      html += '<li class="' + (state.nowCut === item.dy ? "active showing" : "showing") + '" title="' + item.dyjc + '" data-checked="' + item.dy + '">' + item.dyjc + '<span>&times;</span></li>'
+    } else {
+      html += '<li class="' + (state.nowCut === item.dy ? "active" : "") + '" title="' + item.dyjc + '" data-checked="' + item.dy + '">' + item.dyjc + '<span>&times;</span></li>'
+    }
+  })
+  $(".unit_cut_title ul").html(html)
+}
+
+// tab内容切换
+function tabContent() {
+  var data = xnjfSource.list
+  $(".unit_content ul").empty()
+  var nowContentArr = data.filter(function (item) { if (item.dy === state.nowCut) { return item } })
+  if (nowContentArr.length === 1) { nowContent = nowContentArr[0] }
+  var html = ''
+  var lastFloor = {}
+  var lastRoom = {}
+  nowContent.floors.map(function (floorItem, i) {
+    html += '<ul>'
+    html += '<li class="floor-line" data-floor="' + floorItem.floor + ' ">' + floorItem.floor + '层<span>&times;</span></li>'
+    html += '<li class="room-line">'
+    html += '<dl>'
+    floorItem.rooms.map(function (floorChildItem, j) {
+      html += '<dt data-room="' + floorChildItem.fjid + '"  data-location="' + floorChildItem.location + '">' + floorChildItem.roomname + '<span>&times;</span></dt>'
+      if (floorItem.rooms.length === (j + 1)) { lastRoom = floorChildItem }
+    })
+    html += '<dt onclick="addRoom(' + JSON.stringify(floorItem).replace(/\"/g, "'") + ',' + JSON.stringify(lastRoom).replace(/\"/g, "'") + ')">+</dt>'
+    html += '</dl>'
+    html += '</li>'
+    html += '</ul>'
+    if (nowContent.floors.length === (i + 1)) { lastFloor = floorItem }
+  })
+  html += '<ul><li class="floor-line" onclick="addFloor(' + JSON.stringify(lastFloor).replace(/\"/g, "'") + ')">+</li></ul>'
+  $(".unit_content").html(html)
+}
+
+// 事件
+function buttonEvent() {
+  // 删除tab
+  $(".unit_cut_title ul li span").unbind("click").click(function (e) {
+    var dataId = $(this).parent().data("checked")
+    var titleName = $(this).parent().attr("title")
+    var r = confirm(titleName + "该单元包含房间信息，确认删除？");
+    if (r == true) {
+      deleteUnit(dataId)
+    }
+    e.stopPropagation() // 阻止事件冒泡
+  })
+
+  // tab切换
+  $(".unit_cut_title ul li").unbind("click").click(function () {
+    var dataId = $(this).data("checked")
+    $(this).addClass("active").siblings().removeClass("active")
+    state.nowCut = dataId
+    tabContent()
+  })
+
+  // 左右切换tab
+  $(".cut_action").unbind("click").click(function () {
+    if ($(this).hasClass("add_right")) { return }
+    if ($(".unit_cut_title ul li").length <= $(".unit_cut_title ul li.showing").length) {
+      alert("无左右切换操作")
+      return
+    }
+    if ($(this).hasClass("cut_left")) {
+      if ($(".unit_cut_title ul li.showing").eq(0).prev()[0] === "" || $(".unit_cut_title ul li.showing").eq(0).prev()[0] === undefined) {
+        return
+      }
+      $(".unit_cut_title ul li.showing").eq(9).removeClass("showing")
+      $(".unit_cut_title ul li.showing").eq(0).prev().addClass("showing")
+    } else if ($(this).hasClass("cut_right")) {
+      if ($(".unit_cut_title ul li.showing").eq(9).next()[0] === "" || $(".unit_cut_title ul li.showing").eq(9).next()[0] === undefined) {
+        return
+      }
+      $(".unit_cut_title ul li.showing").eq(0).removeClass("showing")
+      $(".unit_cut_title ul li.showing").eq(8).next().addClass("showing")
+    }
+  })
+
+  // 新增楼层
+  $("add_right").unbind("click").click(function () {
+    
+  })
+
+  // 删除楼层
+  $(".floor-line span").unbind("click").click(function (e) {
+    var floorId = $(this).parent().data("floor")
+    console.log(floorId)
+    var r = confirm("删除楼层会删除该楼层所有房间以及关联信息？");
+    if (r == true) {
+      deleteFloor(floorId)
+    }
+    e.stopPropagation() // 阻止事件冒泡
+  })
+
+  // 删除房间
+  $(".room-line dt span").unbind("click").click(function (e) {
+    var roomId = $(this).parent().data("room")
+    console.log(roomId)
+    var r = confirm("删除房间会删除该房间所关联信息，如人员、单位等？");
+    if (r == true) {
+      deleteRoom(roomId)
+    }
+    e.stopPropagation() // 阻止事件冒泡
+  })
+}
+
+// 删除单元
+function deleteUnit(id) {
+  // 接口
+  if (id === state.nowCut) {
+    state.nowCut = xnjfSource && xnjfSource.list && xnjfSource.list[0] ? xnjfSource.list[0].dy : ""
+  }
+  getDataSource()
+}
+
+// 新增楼层
+function addFloor(data) {
+  console.log(data)
+}
+
+// 删除楼层
+function deleteFloor(floorId) {
+
+}
+
+// 新增房间
+function addRoom(floor, room) {
+  console.log(floor)
+  console.log(room)
+}
+
+// 删除房间
+function deleteRoom(roomId) {
+
+}
